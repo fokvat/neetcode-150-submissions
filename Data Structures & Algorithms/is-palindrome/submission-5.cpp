a4b5dfb8b3ef3944791052bcc8b3acdd2fa788a0
Piyush <32892643+fokvat@users.noreply.github.com>
2026-06-24 13:51:18 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,24 @@
+#include <cctype>
+class Solution
+{
+public:
+    bool isPalindrome(string s) {
+         string pal = "";
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end){
+            if(!isalnum(s[start])) start++;
+            else if (!isalnum(s[end])) end --;
+            else if (tolower(s[start]) != tolower(s[end])) return false;
+            else {
+                start++;
+                end --;
+            }
+        }
+        
+
+        return true;
+    }
+};
