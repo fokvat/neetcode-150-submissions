5e9faaa170b28e36709ffe494b6d1024b1359f8c
Piyush <32892643+fokvat@users.noreply.github.com>
2026-07-14 15:32:29 +0530

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+
+        for(string s : tokens){
+            if(!(s == "+" || s == "-" || s == "/" || s == "*")){
+                st.push(stoi(s));
+            }else{
+                int t1 = st.top();
+                st.pop();
+                int t2 = st.top();
+                st.pop();
+                char x  = s[0];
+                switch(x){
+                    case '+': st.push(t2 + t1); break;
+                    case '-': st.push(t2 - t1); break;
+                    case '/': st.push(t2 / t1); break;
+                    case '*': st.push(t2 * t1); break;
+                }
+               
+            }
+        }
+
+        return st.top();
+    }
+};
