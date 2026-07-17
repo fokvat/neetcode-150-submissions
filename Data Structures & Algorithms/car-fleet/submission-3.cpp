42d79bd28caf1f75586bc428a0163e7402e54fcb
Piyush <32892643+fokvat@users.noreply.github.com>
2026-07-17 15:11:12 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, double>> final;
+        stack<int> ans;
+        int carFleet = 0;
+        double lastTime = -1;
+
+        for(int i =0; i < position.size(); i++){
+            double x = (double)(target - position[i]) / speed[i];
+            // cout<< x <<endl;
+            final.push_back({position[i], x});
+        }
+        sort(final.begin(), final.end(), greater<pair<int, int>>());
+
+        for(auto s : final){
+            if(s.second > lastTime){
+                carFleet++;
+                lastTime = s.second;
+            }
+        }
+        
+        return carFleet;
+    }
+};
