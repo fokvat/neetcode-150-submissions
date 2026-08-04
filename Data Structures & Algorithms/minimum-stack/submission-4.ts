f9447c48fb133e701be06bff5511936a86cc4a75
Piyush <32892643+fokvat@users.noreply.github.com>
2026-08-04 20:06:39 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.ts
@@ -0,0 +1,42 @@
+class MinStack {
+    st:number[];
+    min_st:number[];
+
+    constructor() {
+        this.st = [];
+        this.min_st = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.st.push(val);
+        let curMin = this.min_st.at(-1);
+        console.log(curMin);
+        curMin != undefined && curMin < val ? this.min_st.push(curMin) : this.min_st.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.st.pop();
+        this.min_st.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.st.at(-1);
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.min_st.at(-1);
+    }
+}
